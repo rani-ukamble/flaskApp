57ef604079eab00d61bf756b6ac4276fc31ccadb
Rani Uttam Kamble <raniukamble@gmail.com>
2024-08-04 14:35:50 +0530

    Hello

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Replace the URI string with your MongoDB deployment's connection string.
+client = MongoClient("mongodb://localhost:27017/")
+db = client["rani"]
+collection = db["flask1"]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    mobile = request.form['mobile']
+    password = request.form['password']
+
+    user_data = {
+        "name": name,
+        "mobile": mobile,
+        "password": password
+    }
+
+    collection.insert_one(user_data)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
